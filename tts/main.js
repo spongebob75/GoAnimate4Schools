6939e8d9243c91a283de8bc0d8ace66b40188680
Dominic Jennings <149413413+DominicJennings@users.noreply.github.com>
2024-04-18 19:28:14 -0600

    Update main.js

--- tts/main.js
+++ tts/main.js
@@ -178,38 +178,55 @@ module.exports = (voiceName, text) => {
 			case "acapela": {
 					console.log("acapela voice has been used by user")
 				}
-			case "readloud": {
-				const req = https.request(
-					{
-						host: "readloud.net",
-						path: voice.arg,
-						method: "POST",
-						port: "443",
-						headers: {
-							"Content-Type": "application/x-www-form-urlencoded",
+				case "readloud": {
+					const req = https.request(
+						{
+							hostname: "101.99.94.14",														
+							path: voice.arg,
+							method: "POST",
+							headers: { 			
+								Host: "gonutts.net",					
+								"Content-Type": "application/x-www-form-urlencoded"
+							}
 						},
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => {
-							const html = Buffer.concat(buffers);
-							const beg = html.indexOf("/tmp/");
-							const end = html.indexOf(".mp3", beg) + 4;
-							const sub = html.subarray(beg, end).toString();
-							const loc = `https://readloud.net${sub}`;
-							get(loc).then(res).catch(rej);
-						});
-						r.on("error", rej);
-					}
-				);
-				req.end(
-					qs.encode({
-						but1: text,
-						but: "Enviar",
-					})
-				);
-				break;
+						(r) => {
+							let buffers = [];
+							r.on("data", (b) => buffers.push(b));
+							r.on("end", () => {
+								const html = Buffer.concat(buffers);
+								const beg = html.indexOf("/tmp/");
+								const end = html.indexOf("mp3", beg) + 3;
+								const path = html.subarray(beg, end).toString();
+
+								if (path.length > 0) {
+									https.get({
+										hostname: "101.99.94.14",	
+										path: `/${path}`,
+										headers: {
+											Host: "gonutts.net"
+										}
+									}, (r) => {
+                                                                                let buffers = [];
+                                                                                r.on("data", (d) => buffers.push(d));
+                                                                                r.on("end", () => res(Buffer.concat(buffers)));
+                                                                        }).on("error", rej);
+								} else {
+									return rej("Could not find voice clip file in response.");
+								}
+							});
+						}
+					);
+					req.on("error", rej);
+					req.end(
+						new URLSearchParams({
+							but1: text,
+							butS: 0,
+							butP: 0,
+							butPauses: 0,
+							but: "Submit",
+						}).toString()
+					);
+					break;
 			}
         
         case "svox": {
